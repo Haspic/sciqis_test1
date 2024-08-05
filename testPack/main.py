91d3f4dd1db4ca7b9548075d7fef359e132fed60
Arthur Picherit <arthurpicherit95@gmail.com>
2024-08-05 16:18:37 +0200

    added features

--- testPack/main.py
+++ testPack/main.py
@@ -1,12 +1,18 @@
 
+import time
+
 def add(a, b):
+    print(time.time())
     return a+b
 
 def sub(a, b):
+    print(time.time())
     return a-b
 
 def mul(a, b):
+    print(time.time())
     return a*b
 
 def div(a, b):
+    print(time.time())
     return a/b
---
+++ testPack/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='testPack',
+    version='0.1.0',
+    install_requires=[
+        'time'
+    ],
+)
