a97b540d3a60f674ba00e77fa7a65bb9eafc3873
Arthur Picherit <arthurpicherit95@gmail.com>
2024-08-08 09:52:43 +0200

    reset

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
 
-import time
 
-def main_function(n):
-
-    print(n)
-
-    for i in range(n):
-        print(i**n)
-
-    print("[INFO] - Finished at time {}".format(time.time()))
--- testPack/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='testPack',
-    version='0.1.0',
-    install_requires=[
-        'time'
-    ],
-)
